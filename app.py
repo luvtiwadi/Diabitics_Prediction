12633a12e193147c5ec3c066f0a6ea8721bf9b4c
luvtiwadi <lovetiwadi5@gmail.com>
2023-05-12 02:09:52 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,77 @@
-from flask import Flask
+from flask import Flask, request , render_template
+import pickle
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def home():
+    return render_template('home.html')
+
+# @app.route('/math',methods=['POST','GET'])
+
+@app.route('/math',methods=["POST"])
+def inputs():
+    return render_template('input.html')
+
+# @app.route('/result',methods=['POST'])
+
+# @app.route('/result',methods=['POST','GET'])
+# def calculating():
+#     if request.method=='POST':
+
+#         pregnant=request.form['preg']
+#         # pregnant=int(request.form.get('preg'))
+#         glucose=float(request.form['glucose'])
+#         # glucose=request.form.get('glucose')
+
+#         bp=float(request.form['bp'])
+#         # bp=request.form.get('bp')
+
+#         st=float(request.form['st'])
+#         # st=request.form.get('st')
+#         insulin=float(request.form['insulin'])
+#         # insulin=request.form.get('insulin')
+#         bmi=int(request.form['bmi'])
+#         # bmi=request.form.get('bmi')
+#         dpf=float(request.form['dpf'])
+#         # dpf=request.form.get('dpf')
+
+#         age=int(request.form['age'])
+#         # age=request.form.get('age')
+#         # ,res=glucose
+#         # print('pregenant')
+#         return render_template('Result.html',res=pregnant)
+
+
+
+@app.route('/result', methods=['POST'])
+def calculating():
+    if request.method == 'POST':
+        pregnant = int(request.form.get('preg'))
+        glucose = float(request.form.get('glucose'))
+        bp = float(request.form.get('bp'))
+        st = float(request.form.get('st'))
+        insulin = float(request.form.get('insulin'))
+        bmi = float(request.form.get('bmi'))
+        dpf = float(request.form.get('dpf'))
+        age = int(request.form.get('age'))
+
+        # Perform calculations or any other logic here
+        scaler=pickle.load(open("/config/workspace/model/scaler11.pkl","rb"))
+        model=pickle.load(open("/config/workspace/model/regressor11.pkl","rb"))
+        scaled_data=scaler.transform([[pregnant,glucose,bp,st,insulin,bmi,dpf,age]])
+        predict=model.predict(scaled_data)
+
+        if predict[0]==1:
+            result="Diabetic"
+        else:
+            result="Not a Diabitic"
+
+        return render_template('Result.html', res=result)
+    else:
+        return render_template('input.html')
+ 
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
